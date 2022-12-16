5f5b6c91384e916e6e41be045fc8d3e392416a2e
Cameron <cameronkwalden@gmail.com>
2022-12-15 19:45:36 -0800

    adds more Card interaction

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,30 +1,92 @@
-import { Card, CardContent, CardActions, Button, Typography } from "@mui/material";
+import { useState } from "react";
+import {
+  Grid,
+  Card,
+  CardHeader,
+  CardMedia,
+  Collapse,
+  Avatar,
+  IconButton,
+  CardContent,
+  CardActions,
+  Typography,
+} from "@mui/material";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import ShareIcon from "@mui/icons-material/Share";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import { styled } from "@mui/material/styles";
+import { red } from "@mui/material/colors";
+
+const ExpandMore = styled((props) => {
+  const { expand, ...other } = props;
+  return <IconButton {...other} />;
+})(({ theme, expand }) => ({
+  transform: !expand ? "rotate(0deg)" : "rotate(180deg)",
+  marginLeft: "auto",
+  transition: theme.transitions.create("transform", {
+    duration: theme.transitions.duration.shortest,
+  }),
+}));
 
 export default function Movies({ movies }) {
+  const [expanded, setExpanded] = useState(false);
+
+  const handleExpandClick = () => setExpanded(!expanded);
+
   return (
-    <div>
-      {movies.results.map((movie) => (
-        <Card sx={{ minWidth: 275 }}>
-          <CardContent>
-            <Typography
-              sx={{ fontSize: 14 }}
-              color="text.secondary"
-              gutterBottom
-            >
-              {movie.title}
-            </Typography>
-            <Typography sx={{ mb: 1.5 }} color="text.secondary">
-              {movie.release_date}
-            </Typography>
-            <Typography variant="body2" component="div">
-              {movie.overview}
-            </Typography>
-          </CardContent>
-          <CardActions>
-            <Button size="small">favorite</Button>
-          </CardActions>
-        </Card>
+    <Grid
+      container
+      direction="row"
+      justifyContent="center"
+      alignItems="center"
+    >
+      {movies?.results.map((movie) => (
+        <Grid>
+          <Card sx={{ maxWidth: 345 }}>
+            <CardHeader
+              avatar={
+                <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
+                  user
+                </Avatar>
+              }
+              title={movie?.title}
+              subheader={movie?.release_date}
+            />
+            <CardMedia
+              component="img"
+              height="194"
+              image={`https://image.tmdb.org/t/p/w200${movie?.poster_path}`}
+              alt={movie.title}
+              sx={{ padding: "1em 1em 0 1em", objectFit: "contain" }}
+            />
+            <CardContent>
+              <Typography variant="body2" color="text.secondary">
+                vote average: {movie.vote_average}<br/>vote count: {movie.vote_count}
+              </Typography>
+            </CardContent>
+            <CardActions disableSpacing>
+              <IconButton aria-label="add to favorites">
+                <FavoriteIcon />
+              </IconButton>
+              <ExpandMore
+                expand={expanded}
+                onClick={handleExpandClick}
+                aria-expanded={expanded}
+                aria-label="show more"
+              >
+                <ExpandMoreIcon />
+              </ExpandMore>
+            </CardActions>
+            <Collapse in={expanded} timeout="auto" unmountOnExit>
+              <CardContent>
+                <Typography paragraph>{movie.overview}</Typography>
+              </CardContent>
+            </Collapse>
+          </Card>
+        </Grid>
       ))}
-    </div>
+    </Grid>
   );
 }
+
