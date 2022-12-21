f4849aeccdc9639c404d708658dc4f7288e677cc
Cameron <cameronkwalden@gmail.com>
2022-12-21 11:09:27 -0800

    moves nextPage() to Pages, adds prev/next btns logic

--- src/components/Main.js
+++ src/components/Main.js
@@ -8,14 +8,10 @@ export default function Main() {
   const [search, setSearch] = useState("");
   const [movies, setMovies] = useState([]);
   const [totalResults, setTotalResults] = useState(0);
-  const [currentPage, setCurrentPage] = useState(1);
 
   const getMovies = async () => {
     try {
-      let movieResponse = await axios?.get(
-        `http://localhost:3001/movies?title=${search}`
-      );
-
+      let movieResponse = await axios?.get(`http://localhost:3001/movie?title=${search}`);
       setMovies(movieResponse?.data);
       setTotalResults(movieResponse?.data?.total_results);
     } catch (error) {
@@ -23,35 +19,20 @@ export default function Main() {
     }
   };
 
-  const nextPage = async (page) => {
-    try {
-      let movieResponse = await axios?.get(
-        `http://localhost:3001/movies?title=${search}&page=${page}`
-      );
-
-      setMovies(movieResponse?.data);
-      setCurrentPage(page);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
   useEffect(() => {
     getMovies();
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
     <div>
       <Form search={search} setSearch={setSearch} getMovies={getMovies} />
       <Movies movies={movies} />
-      {/* {totalResults > 20 ? ( */}
-        <Pages
-          totalResults={totalResults}
-          currentPage={currentPage}
-          nextPage={nextPage}
-        />
-      {/* ) : null} */}
+      <Pages
+        search={search}
+        setMovies={setMovies}
+        totalResults={totalResults}
+      />
     </div>
   );
 }
--- src/components/Pages.js
+++ src/components/Pages.js
@@ -1,15 +1,49 @@
-export default function Pages({ totalResults, nextPage }) {
-  const pages = [];
-  const moviesShown = Math.floor(totalResults / 20)
+import { useState } from "react";
+import axios from "axios";
+import { Button } from "@mui/material";
+import ArrowCircleLeftIcon from "@mui/icons-material/ArrowCircleLeft";
+import ArrowCircleRightIcon from "@mui/icons-material/ArrowCircleRight";
 
-  for(let i = 1; i <= moviesShown + 1; i++){
-    // eslint-disable-next-line jsx-a11y/anchor-is-valid
-    pages.push(<p key={i} onClick={() => nextPage(i)}><a href="#">{i}</a></p>)
+export default function Pages({ search, setMovies, totalResults }) {
+  const [currentPage, setCurrentPage] = useState(1);
+  const pages = [];
+  const moviesShown = Math.floor(totalResults / 20);
+
+  const nextPage = async (page) => {
+    try {
+      let movieResponse = await axios?.get(`http://localhost:3001/movie?title=${search}&page=${page}`);
+      setMovies(movieResponse?.data);
+      setCurrentPage(page);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  for (let i = 1; i <= moviesShown + 1; i++) {
+    pages.push(
+      <Button key={i} onClick={() => nextPage(i)}>
+        {i}
+      </Button>
+    );
   }
 
   return (
     <div>
-      {totalResults > 20 ? pages : null}
-      </div>
+      {currentPage > 1 ? (
+        <Button onClick={() => nextPage(currentPage - 1)}>
+          <ArrowCircleLeftIcon />
+        </Button>
+      ) : (
+        ""
+      )}
+      {totalResults > 20 ? pages : totalResults <= 1 ? '' : ''}
+      {currentPage < pages + 1 ? (
+        <Button onClick={() => nextPage(currentPage + 1)}>
+          <ArrowCircleRightIcon />
+        </Button>
+      ) : (
+        ""
+      )}
+    </div>
   );
 }
