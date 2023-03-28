eca77d72391d550e540f4d56f1c8dda283af1cf3
Cameron <cameronkwalden@gmail.com>
2023-03-28 14:11:45 -0700

    moves search, setSearch to context

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import App from "./App";
+import Context from "./context/FilmContext";
 import { LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <LocalizationProvider dateAdapter={AdapterDayjs}>
-      <App />
+      <Context>
+        <App />
+      </Context>
     </LocalizationProvider>
   </React.StrictMode>
 );
