763e5379a22a9725a987227aa0df77ba69677011
Cameron <cameronkwalden@gmail.com>
2023-03-28 14:09:06 -0700

    sets up home, saved routes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,18 @@
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import MenuIcon from '@mui/icons-material/Menu';
-import Search from './search/Search';
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import MenuIcon from "@mui/icons-material/Menu";
+import Search from "./search/Search";
+import { Link } from "react-router-dom";
 
-export default function Header({ search, setSearch, getMovies}) {
+export default function Header({ search, setSearch, getMovies }) {
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
-        <Toolbar sx={{ color: 'orange', background: '#2c3440' }}>
+        <Toolbar sx={{ color: "orange", background: "#2c3440" }}>
           <IconButton
             size="large"
             edge="start"
@@ -25,11 +26,17 @@ export default function Header({ search, setSearch, getMovies}) {
             variant="h6"
             noWrap
             component="div"
-            sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
+            sx={{ flexGrow: 1, display: { xs: "none", sm: "block" } }}
           >
             MovieTracker
           </Typography>
-          <Search search={search} setSearch={setSearch} getMovies={getMovies}/>
+          <Link to="/" className="home-route" sx={{ color: "white" }}>
+            Saved
+          </Link>
+          <Link to="/saved-film" className="saved-film" sx={{ color: "white" }}>
+            Saved
+          </Link>
+          <Search search={search} setSearch={setSearch} getMovies={getMovies} />
         </Toolbar>
       </AppBar>
     </Box>
