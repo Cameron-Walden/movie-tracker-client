ae7f6bbd312cbfc21ae929e63e129ff9eb760679
Cameron <cameronkwalden@gmail.com>
2023-03-29 13:32:06 -0700

    moves links to menuIcon, adds slight styling to links

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,27 +1,73 @@
 import * as React from "react";
-import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
-import Toolbar from "@mui/material/Toolbar";
-import IconButton from "@mui/material/IconButton";
-import Typography from "@mui/material/Typography";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import {
+  AppBar,
+  Box,
+  Toolbar,
+  IconButton,
+  Typography,
+  Menu,
+  MenuItem,
+  Fade,
+} from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
 import Search from "./search/Search";
-import { Link } from "react-router-dom";
 
 export default function Header() {
+  const [isOpen, setIsOpen] = useState(null);
+
+  const handleOpenMenu = (e) => setIsOpen(e.currentTarget);
+
+  const handleCloseMenu = () => setIsOpen(null);
+
+  const openMenu = Boolean(isOpen);
+
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
         <Toolbar sx={{ color: "orange", background: "#2c3440" }}>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="open drawer"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
+          <div>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="open drawer"
+              sx={{ mr: 2 }}
+              id="fade-button"
+              aria-controls={openMenu ? "fade-menu" : undefined}
+              aria-haspopup="true"
+              aria-expanded={openMenu ? "true" : undefined}
+              onClick={handleOpenMenu}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="fade-menu"
+              MenuListProps={{
+                "aria-labelledby": "fade-button",
+              }}
+              anchorEl={isOpen}
+              open={openMenu}
+              onClose={handleCloseMenu}
+              TransitionComponent={Fade}
+            >
+              <Link
+                to="/"
+                className="home-route"
+                style={{ textDecoration: "none", color: "black" }}
+              >
+                <MenuItem onClick={handleCloseMenu}>Home</MenuItem>
+              </Link>
+              <Link
+                to="/saved-film"
+                className="saved-film"
+                style={{ textDecoration: "none", color: "black" }}
+              >
+                <MenuItem onClick={handleCloseMenu}>Saved Films</MenuItem>
+              </Link>
+            </Menu>
+          </div>
           <Typography
             variant="h6"
             noWrap
@@ -30,12 +76,6 @@ export default function Header() {
           >
             MovieTracker
           </Typography>
-          <Link to="/" className="home-route" sx={{ color: "white" }}>
-            Home
-          </Link>
-          <Link to="/saved-film" className="saved-film" sx={{ color: "white" }}>
-            Saved
-          </Link>
           <Search />
         </Toolbar>
       </AppBar>
