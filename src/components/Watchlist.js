744732bdca34df61af97de381e03e64e0dbaf75a
Cameron <cameronkwalden@gmail.com>
2023-04-05 10:33:26 -0700

    adds popover for films, adds styling

--- src/components/Watchlist.js
+++ src/components/Watchlist.js
@@ -3,8 +3,9 @@ import axios from "axios";
 import { styled } from "@mui/material/styles";
 import Paper from "@mui/material/Paper";
 import Grid from "@mui/material/Grid";
-import { IconButton } from "@mui/material";
+import { IconButton, Typography } from "@mui/material";
 import VisibilityIcon from "@mui/icons-material/Visibility";
+import Popover from "@mui/material/Popover";
 
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
@@ -16,12 +17,40 @@ const Item = styled(Paper)(({ theme }) => ({
 
 export default function Watchlist() {
   const [watchlist, setWatchlist] = useState([]);
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const handlePopoverOpen = (e, id) => {
+    setAnchorEl(e.currentTarget);
+    const updatedWatchlist = watchlist.map((film) => {
+      if (film._id === id) {
+        return { ...film, popoverOpen: true };
+      }
+      return film;
+    });
+    setWatchlist(updatedWatchlist);
+  };
+
+  const handlePopoverClose = (id) => {
+    const updatedWatchlist = watchlist.map((film) => {
+      if (film._id === id) {
+        return { ...film, popoverOpen: false };
+      }
+      return film;
+    });
+    setWatchlist(updatedWatchlist);
+    setAnchorEl(null);
+  };
+
+  const open = Boolean(anchorEl);
 
   const getWatchlist = async () => {
     try {
       let response = await axios.get("http://localhost:3001/watchlist");
-      console.log(response.data, "res");
-      setWatchlist(response.data);
+      const updatedWatchlist = response.data.map((film) => ({
+        ...film,
+        popoverOpen: false,
+      }));
+      setWatchlist(updatedWatchlist);
     } catch (error) {
       console.log(error);
     }
@@ -42,29 +71,66 @@ export default function Watchlist() {
   }, []);
 
   return (
-    <Grid
-      container
-      spacing={{ xs: 2, md: 3 }}
-      columns={{ xs: 4, sm: 8, md: 12 }}
-      direction="row"
-      alignItems="center"
-      justifyContent="center"
-    >
-      {watchlist.map((film) => (
-        <Item>
-          <img
-            src={`https://image.tmdb.org/t/p/w500/${film.poster}`}
-            alt={film.title}
-            style={{ width: "12em", height: "20em" }}
-          />
-          <IconButton
-            aria-label="remove from watchlist"
-            onClick={() => removeFromWatchlist(film._id)}
-          >
-            <VisibilityIcon />
-          </IconButton>
-        </Item>
-      ))}
-    </Grid>
+    <>
+      <div style={{ paddingLeft: "3em", paddingTop: "3em" }}>
+        <Typography>You want to see {watchlist.length} movies</Typography>
+      </div>
+      <Grid
+        container
+        rowSpacing={1}
+        columnSpacing={{ xs: 12, sm: 12, md: 12 }}
+        spacing={{ xs: 12, md: 12 }}
+        columns={{ xs: 7, sm: 12, md: 12 }}
+        direction="row"
+        alignItems="center"
+        justifyContent="center"
+        paddingTop="3em"
+      >
+        {watchlist.map((film) => (
+          <div key={film._id}>
+            <Item
+              aria-owns={open ? "mouse-over-popover" : undefined}
+              aria-haspopup="true"
+              onMouseEnter={(e) => handlePopoverOpen(e, film?._id)}
+              onMouseLeave={() => handlePopoverClose(film._id)}
+            >
+              <Popover
+                id="mouse-over-popover"
+                sx={{
+                  pointerEvents: "none",
+                }}
+                open={film.popoverOpen}
+                anchorEl={anchorEl}
+                anchorOrigin={{
+                  vertical: "top",
+                  horizontal: "center",
+                }}
+                transformOrigin={{
+                  vertical: "top",
+                  horizontal: "center",
+                }}
+                onClose={() => handlePopoverClose(film._id)}
+                disableRestoreFocus
+              >
+                {film.title && film._id && (
+                  <Typography sx={{ p: 1 }}>{film.title}.</Typography>
+                )}
+              </Popover>
+              <img
+                src={`https://image.tmdb.org/t/p/w500/${film?.poster}`}
+                alt={film.title}
+                style={{ width: "12em", height: "20em" }}
+              />
+              <IconButton
+                aria-label="remove from watchlist"
+                onClick={() => removeFromWatchlist(film._id)}
+              >
+                <VisibilityIcon />
+              </IconButton>
+            </Item>
+          </div>
+        ))}
+      </Grid>
+    </>
   );
 }
