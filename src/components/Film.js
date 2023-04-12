72277c58f77af7ebacab1c3c2696758288e9c002
Cameron <cameronkwalden@gmail.com>
2023-04-12 14:20:13 -0700

    can succesfully route to /film/:id, adds some styling

--- src/components/Film.js
+++ src/components/Film.js
@@ -1,8 +1,59 @@
-export default function Film(){
+import { useState, useContext, useEffect } from "react";
+import axios from "axios";
+import { FilmContext } from "../context/FilmContext";
+import { useParams } from "react-router-dom";
+import { Container, padding } from "@mui/system";
+import "./Film.css";
 
-    return(
-        <div>
-            <p>in film route</p>
-        </div>
-    )
+export default function Film() {
+  const [filmId, setFilmId] = useState(null);
+
+  const { id } = useParams();
+
+  const { search } = useContext(FilmContext);
+
+  const getFilm = async () => {
+    try {
+      let movieResponse = await axios?.get(
+        // `https://api.themoviedb.org/3/search/movie/${id}?api_key=${process.env.REACT_APP_MOVIE_API}&query=${search}`
+        `https://api.themoviedb.org/3/movie/${id}?api_key=${process.env.REACT_APP_MOVIE_API}`
+      );
+      console.log(movieResponse.data, "movieRes");
+      setFilmId(movieResponse?.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getFilm();
+  }, [id]);
+
+  return (
+    <div>
+      {filmId && (
+        <Container>
+          <div>
+            <img
+              src={`https://image.tmdb.org/t/p/w1280/${filmId.backdrop_path}`}
+              alt={filmId.title}
+              className="movie-backdrop"
+            />
+          </div>
+          <div className="poster-container">
+            <div className="poster-details">
+              <img
+                src={`https://image.tmdb.org/t/p/w342/${filmId.poster_path}`}
+                alt={filmId.title}
+              />
+              <div>
+                <p className="film-title">{filmId.title}</p>
+                <p className="film-overview">{filmId.overview}</p>
+              </div>
+            </div>
+          </div>
+        </Container>
+      )}
+    </div>
+  );
 }
