a0a0cf0af96a30edacaf660f2fa56aac4eb111b1
Cameron <cameronkwalden@gmail.com>
2023-05-04 15:32:49 -0700

    removes mui Item component/styling/, centers posters, adds styling via css

--- src/components/filmsByGenre/FilmsByGenre.js
+++ src/components/filmsByGenre/FilmsByGenre.js
@@ -1,29 +1,27 @@
 import { useContext } from 'react';
 import { Link } from 'react-router-dom';
-import { Item } from './genreStyles';
 import { Grid } from '@mui/material';
 import { FilmContext } from '../../context/FilmContext';
+import './FilmsByGenre.css'
 
 export default function FilmsByGenre() {
   const { ddMovies } = useContext(FilmContext)
   return (
     <>
-      <Grid className="film-grid" container rowSpacing={3} columnSpacing={2}>
+      <Grid className="film-grid" container >
         {ddMovies.map((film) => (
-          <div key={film._id}>
+          <div key={film._id} className="img-container">
             {console.log(film, "film")}
             <Link to={`/film/${film.id}`} className="movie-link">
-            <Item>
               <img
                 src={`https://image.tmdb.org/t/p/w500/${film?.poster_path}`}
                 alt={film.title}
                 style={{
                   width: "12em",
                   height: "20em",
-                  justifyContent: "space-around",
+                  paddingLeft: "5px"
                 }}
               />
-            </Item>
             </Link>
           </div>
         ))}
