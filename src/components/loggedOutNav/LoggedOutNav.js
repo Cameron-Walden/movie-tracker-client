8344dfc4df932069f29e3170b9d76b4887df4171
Cameron <cameronkwalden@gmail.com>
2023-05-31 16:03:45 -0700

    adds Link to movie tracker logo to return home when logged out

--- src/components/loggedOutNav/LoggedOutNav.js
+++ src/components/loggedOutNav/LoggedOutNav.js
@@ -1,4 +1,5 @@
 import { AppBar, Box, Toolbar, Typography } from "@mui/material";
+import { Link } from "react-router-dom";
 import LoginButton from "../loginButton/LoginButton";
 import Search from "../search/Search";
 
@@ -13,7 +14,9 @@ export default function LoggedOutNav() {
             component="div"
             sx={{ flexGrow: 1, display: { xs: "none", sm: "block" } }}
           >
-            MovieTracker
+            <Link to="/" className="home-route" style={{ color: "orange" }}>
+              MovieTracker
+            </Link>
           </Typography>
           <LoginButton />
           <Search />
