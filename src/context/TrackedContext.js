031b9cfc3a2a728718babc9c3c903a9d630fe878
Cameron <cameronkwalden@gmail.com>
2024-03-23 11:13:41 -0700

    extracts tracked film related state into separate TrackedContext

--- src/context/TrackedContext.js
+++ src/context/TrackedContext.js
@@ -5,9 +5,18 @@ export default function TrackedProvider(props) {
   const [trackedMovies, setTrackedMovies] = useState([]);
   const [openEdit, setOpenEdit] = useState(false);
   const [selectedMovieEdit, setSelectedMovieEdit] = useState(null);
-  
+
   return (
-    <TrackedContext.Provider value={{ trackedMovies, setTrackedMovies }}>
+    <TrackedContext.Provider
+      value={{
+        trackedMovies,
+        setTrackedMovies,
+        openEdit,
+        setOpenEdit,
+        selectedMovieEdit,
+        setSelectedMovieEdit,
+      }}
+    >
       {props.children}
     </TrackedContext.Provider>
   );
